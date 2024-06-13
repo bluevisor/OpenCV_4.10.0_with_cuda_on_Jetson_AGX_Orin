6358bdc68992490bbac80ab5528c7b6d0a882b7b
John Zheng <bluevisor@gmail.com>
2024-06-13 02:05:20 -0700

    Update install_OpenCV_4.10.0_with_cuda_on_Jetpack_6.sh

--- install_OpenCV_4.10.0_with_cuda_on_Jetpack_6.sh
+++ install_OpenCV_4.10.0_with_cuda_on_Jetpack_6.sh
@@ -9,6 +9,7 @@
 # license agreement from NVIDIA Corporation is strictly prohibited.
 #
 # Modified from this script: https://github.com/AastaNV/JEP/blob/master/script/install_opencv4.6.0_Jetson.sh
+# Remember to change power setting to MAXN first.
 
 version="4.10.0"
 folder="workspace"
@@ -36,9 +37,8 @@ echo "------------------------------------"
 sudo apt-get update
 sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
 sudo apt-get install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-#sudo apt-get install -y python3.8-dev python-dev python-numpy python3-numpy
-sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev #libdc1394-22-dev
-sudo apt-get install -y libv4l-dev v4l-utils qv4l2 #v4l2ucp
+sudo apt-get install -y libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get install -y libv4l-dev v4l-utils qv4l2
 sudo apt-get install -y curl
 
 
